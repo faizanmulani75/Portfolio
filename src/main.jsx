bf94d68c71b0a87dae21a72d2d5c533d60c49b50
Faizan Mulani <faizanmulani1810@gmail.com>
2026-01-18 23:45:52 +0530

    Fix Some bugs

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import "sheryjs/dist/Shery.css";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
